7c9aedf31b2c3bdefc8bf4ace4f640c161a442a7
John Wood <johna613@gmail.com>
2023-01-31 12:45:27 -0500

    adding file

---
+++ exercise10.3.js
@@ -0,0 +1,8 @@
+function double(arr){
+	arr.map((val) => val * 2)
+}
+
+const double = arr.map(val => val * 2);
+
+
+const squareAndFindEvens = (numbers) => numbers.map((num) => num ** 2).filter((square) => square % 2 === 0)
